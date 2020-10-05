2d652ffe1c1aa2169087500700f3aa7b542e47f2
Dhwaniparekh-hub <61867063+Dhwaniparekh-hub@users.noreply.github.com>
2020-10-05 12:35:15 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+class Calculator:
+
+    def __init__(self, master):
+
+        self.master = master
+
+
+        master.title("Python Calculator")
+
+        self.equation=Entry(master, width=36, borderwidth=5)
+        self.equation.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+        self.createButton()
